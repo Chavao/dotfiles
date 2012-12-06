9ec80ec2fe685ca1dbb65641dc9d8f5cf8dbade2
Chavão <fale@chavao.net>
2012-12-06 17:31:30 -0200

    Remove usless functions

--- functions/cakemode.sh
+++
@@ -1,7 +0,0 @@
-# Put the current project as a CakePHP project in the PATH
-function cakemode() {
-    DIR=$(pwd)'/app/Console'
-    if [ -d "$DIR" ]; then
-        PATH="$PATH:$DIR"
-    fi
-}
--- functions/git.sh
+++
@@ -1,6 +0,0 @@
-function git_prompt() {
-    if [ $(__git_ps1) ]; then
-        printf ' '
-        printf $(__git_ps1)
-    fi
-}
