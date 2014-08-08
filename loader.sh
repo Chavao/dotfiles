b88c042e4685728362cb2ec800d088d580f06b64
Diego Chavão <diego.chavao@concretesolutions.com.br>
2014-08-08 13:45:30 -0300

    Improve shell checking

--- loader.sh
+++ loader.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [[ $(ps -p $$ | tail -n1) == *"bash"* ]]; then source ~/dotfiles/bashrc
+if [[ $(ps -p $$ -ocomm=) == "bash" ]]; then source ~/dotfiles/bashrc
 else source ~/dotfiles/zshrc
 fi
