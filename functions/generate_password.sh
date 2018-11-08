a6d3d36ccf38d4a7c4b502e7ca2169bd2a765a21
Diego Chavão <d.chavao@personare.com.br>
2018-11-08 11:39:44 -0200

    Add alias to generate_password function.
    - genpass

--- functions/generate_password.sh
+++ functions/generate_password.sh
@@ -2,3 +2,4 @@ function generate_password {
     p_pass=$(head /dev/urandom | base64 | awk '{ sub ("\\\\$", ""); printf "%s", $0 } END { print "" }')
     echo ${p_pass:0:499}
 }
+alias genpass='generate_password'
