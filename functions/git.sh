aa0c78fb8b2a08856d4c9dc141732deb7d0e73af
Diego Chavão <diego.chavao@corp.globo.com>
2018-12-18 16:16:54 -0200

    Add function to run git push --force to current branch.
    Asks before push!

--- functions/git.sh
+++ functions/git.sh
@@ -2,3 +2,17 @@ function push {
    echo "git push -u origin $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)"
    git push -u origin $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 }
+
+function pushf() {
+   printf "Force push to branch $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)? [y/N]: "
+   read PUSH_FORCE_BRANCH
+
+   if [ ! -z $PUSH_FORCE_BRANCH ]
+   then
+      if [ $PUSH_FORCE_BRANCH == 'y' ]
+      then
+         echo "git push --force -u origin $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)"
+         git push --force -u origin $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+       fi
+    fi
+}
