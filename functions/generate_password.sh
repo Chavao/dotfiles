bc45632a2f034ae57f8842c7b19868b782ee3bbd
Diego Chavão <diego.chavao@corp.globo.com>
2019-01-24 17:01:32 -0200

    Add limit to password generator.

--- functions/generate_password.sh
+++ functions/generate_password.sh
@@ -1,5 +1,10 @@
 function generate_password {
+    if [ -z "$1" ]; then
+        FINAL=499
+    else
+        FINAL=$1
+    fi
     p_pass=$(head /dev/urandom | base64 | awk '{ sub ("\\\\$", ""); printf "%s", $0 } END { print "" }')
-    echo ${p_pass:0:499}
+    echo ${p_pass:0:$FINAL}
 }
 alias genpass='generate_password'
