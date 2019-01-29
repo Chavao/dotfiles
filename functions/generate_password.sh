005628aad821ed817311bbaf83c095d48f849660
Diego Chavão <diego.chavao@corp.globo.com>
2019-01-29 18:19:27 -0200

    Fix script to generate big passwords.

--- functions/generate_password.sh
+++ functions/generate_password.sh
@@ -4,7 +4,7 @@ function generate_password {
     else
         FINAL=$1
     fi
-    p_pass=$(head /dev/urandom | base64 | awk '{ sub ("\\\\$", ""); printf "%s", $0 } END { print "" }')
+    p_pass=$(head -n$FINAL /dev/urandom | base64 | awk '{ sub ("\\\\$", ""); printf "%s", $0 } END { print "" }')
     echo ${p_pass:0:$FINAL}
 }
 alias genpass='generate_password'
