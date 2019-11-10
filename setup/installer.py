7e3eb636eb34b360619a8ddf6b7e97645e442b03
Diego Chavão <diegochavao@gmail.com>
2019-11-09 23:33:58 -0300

    Add functions to handle actions

--- setup/installer.py
+++ setup/installer.py
@@ -1,5 +1,30 @@
 from PyInquirer import prompt
 
+
+def git_config():
+    print("Installing git config")
+
+
+def gitignore_global():
+    print("Intalling gitignore global")
+
+
+def vimfiles():
+    print("Installing vimfiles")
+
+
+def prezto():
+    print("Installing prezto")
+
+
+_FUNCTIONS = {
+    'git_config': git_config,
+    'gitignore_global': gitignore_global,
+    'vimfiles': vimfiles,
+    'prezto': prezto,
+}
+
+
 modules_select = [
     {
         'type': 'checkbox',
@@ -27,3 +52,5 @@ modules_select = [
 ]
 
 modules = prompt(modules_select)
+for module in modules.get('modules'):
+    _FUNCTIONS.get(module)()
