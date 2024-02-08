91a300ab56cd590ad500c270baf4dff5c7f44334
Diego Chavão <fale@chavao.net>
2024-02-08 13:34:00 -0300

    Add function to kill all docker containers

--- functions/docker.sh
+++ functions/docker.sh
@@ -4,3 +4,10 @@ function kdocker {
     echo "Removing docker container: $1"
     docker rm $1 | xargs echo "Removed"
 }
+
+
+function kdockerall {
+    for id in `docker ps -q`; do
+        kdocker $id
+    done
+}
